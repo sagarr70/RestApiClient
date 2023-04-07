ad6c8d10e273930525241a35ec2d808b5b97f637
sagar aggarwal <sagar_aggarwal@outlook.com>
2023-04-07 12:35:44 +0530

    Rest Api Get Response Client

---
+++ RestApiClient/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RestApiClient;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Sample Api endpoint https://api.sampleapis.com/beers/al
+        Console.WriteLine("Enter EndPoint : ");
+        
+        var client = new RestApiClient();
+        client.endPoint = Console.ReadLine();
+        var response =await client.makeRequest();
+        Console.WriteLine(response.ToString());
+        Console.Read();
+
+    }
+}
---
+++ RestApiClient/RestApiClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace RestApiClient
+{
+	public class RestApiClient
+	{
+		public string endPoint { get; set; }
+
+        private static readonly HttpClient client = new HttpClient();
+
+
+        public RestApiClient()
+		{
+        }
+        #region GetRequest
+
+        public async Task<string> makeRequest()
+		{
+            try
+            {
+                string responseBody = await client.GetStringAsync(this.endPoint);
+
+                return responseBody;
+            }
+            catch (Exception e)
+            {
+                throw new HttpRequestException(e.ToString());
+            }
+        }
+        #endregion
+    }
+}
+
